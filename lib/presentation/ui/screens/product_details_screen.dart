8ac723a14cf93c690acbb13a4c7e0bdd09af9275
Jahid01234 <mdjahidhasan790523@gmail.com>
2024-09-12 12:07:40 +0600

    -add product image slider, reviews section

---
+++ lib/presentation/ui/screens/product_details_screen.dart
@@ -0,0 +1,106 @@
+import 'package:crafty_bay/presentation/ui/utils/colors/app_colors.dart';
+import 'package:crafty_bay/presentation/ui/widgets/product_image_slider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:item_count_number_button/item_count_number_button.dart';
+
+class ProductDetailsScreen extends StatefulWidget {
+  const ProductDetailsScreen({super.key});
+
+  @override
+  State<ProductDetailsScreen> createState() => _ProductDetailsScreenState();
+}
+
+class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Product Details"),
+      ),
+      body: Column(
+        children: [
+           const ProductImageSlider(),
+           Padding(
+             padding: const EdgeInsets.all(15.0),
+             child: Column(
+               crossAxisAlignment: CrossAxisAlignment.start,
+               children: [
+                 Row(
+                   crossAxisAlignment: CrossAxisAlignment.start,
+                   children: [
+                     Expanded(
+                       child: Text('Nike shoe 2024 latest model - New year special deal',
+                         style: Theme.of(context).textTheme.titleMedium,
+                       ),
+                     ),
+                     ItemCount(
+                         color: AppColors.themeColor.withOpacity(0.4),
+                         initialValue: 1,
+                         minValue: 1,
+                         maxValue: 20,
+                         decimalPlaces: 0,
+                         onChanged: (value){
+                           setState(() {});
+                         },
+                     ),
+                   ],
+                 ),
+                 const SizedBox(height: 4),
+                 Wrap(
+                   crossAxisAlignment: WrapCrossAlignment.center,
+                   children: [
+                     const Wrap(
+                       crossAxisAlignment: WrapCrossAlignment.center,
+                       children: [
+                         Icon(
+                           Icons.star,
+                           color: Colors.amber,
+                           size: 20,
+                         ),
+                         Text(
+                           "3.4 ",
+                           style: TextStyle(
+                             fontWeight: FontWeight.w500,
+                             color: Colors.black45,
+                           ),
+                         ),
+                       ],
+                     ),
+                     const SizedBox(width: 8),
+                     TextButton(
+                       onPressed: (){},
+                       child: const Text(
+                         "Reviews",
+                         style: TextStyle(
+                           fontWeight: FontWeight.w500,
+                           color:AppColors.themeColor,
+                         ),
+                       ),
+                     ),
+                     const SizedBox(width: 8),
+                     Card(
+                       color: AppColors.themeColor,
+                       shape: RoundedRectangleBorder(
+                         borderRadius: BorderRadius.circular(4),
+                       ),
+                       child: const Padding(
+                         padding: EdgeInsets.all(4.0),
+                         child: Icon(
+                           Icons.favorite_border_rounded,
+                           color: Colors.white,
+                           size: 16,
+                         ),
+                       ),
+                     ),
+                   ],
+                 ),
+               ],
+             ),
+           ),
+
+        ],
+      ),
+    );
+  }
+}
--- lib/presentation/ui/widgets/product_card.dart
+++ lib/presentation/ui/widgets/product_card.dart
@@ -1,6 +1,8 @@
+import 'package:crafty_bay/presentation/ui/screens/product_details_screen.dart';
 import 'package:crafty_bay/presentation/ui/utils/assets/app_assets.dart';
 import 'package:crafty_bay/presentation/ui/utils/colors/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ProductCard extends StatelessWidget {
   const ProductCard({
@@ -9,91 +11,96 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.white,
-      elevation: 1,
-      child: SizedBox(
-        width: 130,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 100,
-              width: 130,
-              padding: const EdgeInsets.all(16),
-              decoration:  BoxDecoration(
-                  color: AppColors.themeColor.withOpacity(0.1),
-                  borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10)
-                  ),
-                  image: const DecorationImage(
-                      image: AssetImage(
-                        AssetsPath.dummyProductImg,
-                      ),
-                    fit: BoxFit.scaleDown
-                  ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text("Product Name ",
-                    maxLines: 1,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      color: Colors.black45,
+    return GestureDetector(
+      onTap: (){
+        Get.to(()=> const ProductDetailsScreen());
+      },
+      child: Card(
+        color: Colors.white,
+        elevation: 1,
+        child: SizedBox(
+          width: 130,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 100,
+                width: 130,
+                padding: const EdgeInsets.all(16),
+                decoration:  BoxDecoration(
+                    color: AppColors.themeColor.withOpacity(0.1),
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10)
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        "\$120 ",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          color:AppColors.themeColor,
+                    image: const DecorationImage(
+                        image: AssetImage(
+                          AssetsPath.dummyProductImg,
                         ),
+                      fit: BoxFit.scaleDown
+                    ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Product Name ",
+                      maxLines: 1,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black45,
                       ),
-                      const Wrap(
-                        crossAxisAlignment: WrapCrossAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.star,
-                            color: Colors.amber,
-                            size: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          "\$120 ",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color:AppColors.themeColor,
                           ),
-                          Text(
-                            "3.4 ",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              color: Colors.black45,
+                        ),
+                        const Wrap(
+                          crossAxisAlignment: WrapCrossAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.star,
+                              color: Colors.amber,
+                              size: 20,
+                            ),
+                            Text(
+                              "3.4 ",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black45,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Card(
+                          color: AppColors.themeColor,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(4),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(4.0),
+                            child: Icon(
+                              Icons.favorite_border_rounded,
+                              color: Colors.white,
+                              size: 16,
                             ),
                           ),
-                        ],
-                      ),
-                      Card(
-                        color: AppColors.themeColor,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(4),
                         ),
-                        child: const Padding(
-                          padding: EdgeInsets.all(4.0),
-                          child: Icon(
-                            Icons.favorite_border_rounded,
-                            color: Colors.white,
-                            size: 16,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
